54cee779aa000eb9f55e7239c7cfc5034888b7fd
Sheikh Farid <mdfaridsagor101@gmail.com>
2024-08-22 11:16:45 +0600

    added nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ['*'],
   theme: {
     extend: {},
+    fontFamily: {
+    'monrope': ['Manrope', 'sans-serif'],
+    },
+    colors: {
+      'primary_btn': '#ABEF5F',
+      'black' : '#000',
+      'white' : '#fff',
+    },
   },
   plugins: [
     require('daisyui'),
