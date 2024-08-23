1e0a3962db57996292725021bae6255b249ef3ad
Sheikh Farid <mdfaridsagor101@gmail.com>
2024-08-23 12:13:33 +0600

    added discuss section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,27 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+       backgroundImage: {
+      'form_image': "url('./assets/bg.jpg')",
+    },
+    },
     fontFamily: {
-    'monrope': ['Manrope', 'sans-serif'],
+      'monrope': ['Manrope', 'sans-serif'],
     },
     colors: {
       'primary_btn': '#ABEF5F',
-      'black' : '#000',
-      'white' : '#fff',
-      'background' : '#F4F4F4',
-      'pink' : '#E7C1D3',
-      'amber' : '#EFDA6E',
-      'emerald' : '#A4DAC3',
-      'blue' : '#77AAEA',
-      'border_color' : '#E9E9E9',
+      'black': '#000',
+      'white': '#fff',
+      'background': '#F4F4F4',
+      'pink': '#E7C1D3',
+      'amber': '#EFDA6E',
+      'emerald': '#A4DAC3',
+      'blue': '#77AAEA',
+      'border_color': '#E9E9E9',
     },
+   
+
   },
   plugins: [
     require('daisyui'),
